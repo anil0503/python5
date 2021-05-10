f26f8aa9675ef6204ceaa1d61594158744923c74
anil0503 <39275440+anil0503@users.noreply.github.com>
2021-05-10 13:06:13 +0530

    Update anil.py

--- anil.py
+++ anil.py
@@ -1,37 +1,22 @@
-# define functions  
-def add(x, y):  
-   """This function adds two numbers"" 
-   return x + y 
-def subtract(x, y): 
-   """This function subtracts two numbers""" 
-   return x - y 
-def multiply(x, y): 
-   """This function multiplies two numbers""" 
-   return x * y 
-def divide(x, y): 
-   """This function divides two numbers"""  
-   return x / y  
-# take input from the user  
-print("Select operation.")  
-print("1.Add")  
-print("2.Subtract")  
-print("3.Multiply")  
-print("4.Divide")  
-  
-choice = input("Enter choice(1/2/3/4):")  
-  
-num1 = int(input("Enter first number: "))  
-num2 = int(input("Enter second number: "))  
-  
-if choice == '1':  
-   print(num1,"+",num2,"=", add(num1,num2))  
-  
-elif choice == '2':  
-   print(num1,"-",num2,"=", subtract(num1,num2))  
-  
-elif choice == '3':  
-   print(num1,"*",num2,"=", multiply(num1,num2))  
-elif choice == '4':  
-   print(num1,"/",num2,"=", divide(num1,num2))  
-else:  
-   print("Invalid input")  
+operation = input('''
+Please type in the math operation you would like to complete:
++ for addition
+- for subtraction
+* for multiplication
+/ for division
+''')
+
+number_1 = int(input('Enter your first number: '))
+number_2 = int(input('Enter your second number: '))
+
+print('{} + {} = '.format(number_1, number_2))
+print(number_1 + number_2)
+
+print('{} - {} = '.format(number_1, number_2))
+print(number_1 - number_2)
+
+print('{} * {} = '.format(number_1, number_2))
+print(number_1 * number_2)
+
+print('{} / {} = '.format(number_1, number_2))
+print(number_1 / number_2)
