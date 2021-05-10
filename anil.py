72d928e12bd18428c3c367207c40daa72b0e079c
anil0503 <39275440+anil0503@users.noreply.github.com>
2021-05-10 12:40:56 +0530

    Create anil.py

---
+++ anil.py
@@ -0,0 +1,14 @@
+import
+List1 = [8, 9, 3, 6, 1, 10]
+list1.reverse()
+Print("The reversed list is", list1)
+
+List1 = [91, 67, 120, 34, 76, 54, 78, 87, 56, 64, 345]
+list2.sort()
+Print("The sorted list is", list2)
+
+list3 = []
+list3 = list.copy()
+Print("list3 =", list3)
+indexvaluen = list2[2:6]
+Print("the index value are" , indexvalue)
