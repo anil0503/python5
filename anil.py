44b6e63ed309da23d9f0bfdaadb72085776b3337
anil0503 <39275440+anil0503@users.noreply.github.com>
2021-05-10 13:22:12 +0530

    Update anil.py

--- anil.py
+++ anil.py
@@ -1,19 +1,21 @@
-num_1 = int(input('Enter your first number: '))
-num_2 = int(input('Enter your second number: '))
- 
-# Addition
-print('{} + {} = '.format(num_1, num_2))
-print(num_1 + num_2)
- 
-# Subtraction
-print('{} - {} = '.format(num_1, num_2))
-print(num_1 - num_2)
- 
-# Multiplication
-print('{} * {} = '.format(num_1, num_2))
-print(num_1 * num_2)
- 
-# Division
-print('{} / {} = '.format(num_1, num_2))
-print(num_1 / num_2)
-# The format() will help out output look descent and formatted.
+# Prints list of random items of
+# length 3 from the given list.
+list1 = [1, 2, 3, 4, 5, 6]
+print("With list:", random.sample(list1, 3))
+
+# Prints list of random items of
+# length 4 from the given string.
+string = "GeeksforGeeks"
+print("With string:", random.sample(string, 4))
+
+# Prints list of random items of
+# length 4 from the given tuple.
+tuple1 = ("ankit", "geeks", "computer", "science",
+				"portal", "scientist", "btech")
+print("With tuple:", random.sample(tuple1, 4))
+
+
+# Prints list of random items of
+# length 3 from the given set.
+set1 = {"a", "b", "c", "d", "e"}
+print("With set:", random.sample(set1, 3))
