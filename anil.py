221cbca730921908369566c46e30dd0ef6391c3b
anil0503 <39275440+anil0503@users.noreply.github.com>
2021-05-10 12:58:13 +0530

    Update anil.py

--- anil.py
+++ anil.py
@@ -1,14 +1,3 @@
-import OS
-List1 = [8, 9, 3, 6, 1, 10]
-list1.reverse()
-Print("The reversed list is", list1)
+# This program prints Hello, world!
 
-List1 = [91, 67, 120, 34, 76, 54, 78, 87, 56, 64, 345]
-list2.sort()
-Print("The sorted list is", list2)
-
-list3 = []
-list3 = list.copy()
-Print("list3 =", list3)
-indexvaluen = list2[2:6]
-Print("the index value are" , indexvalue)
+print('Hello, world!')
